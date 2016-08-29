5cf7b02d2d670024bafd8b5fc90d0f4559388060
Melanie Baker <melanierbaker@yahoo.com>
2016-08-29 14:40:59 -0500

    fixed error

--- 13line_item_track_artist.sql
+++ 13line_item_track_artist.sql
@@ -1,7 +1,11 @@
 /*Provide a query that includes the purchased track name 
 AND artist name with each invoice line item.*/
 
-SELECT   l.InvoiceLineId,  t.Name, a.Name
-From InvoiceLine l, track t, artist a
+SELECT   l.InvoiceLineId as InvoiceNumber,  
+	t.Name as TrackName, 
+	a.Name as ArtistName
+From InvoiceLine l, track t, album al, artist a
 WHERE t.TrackId = l.TrackId
-ORDER BY  a.Name
+and t.AlbumId = al.AlbumId
+and al.ArtistId = a.ArtistId
+ORDER BY  l.InvoiceLineId
